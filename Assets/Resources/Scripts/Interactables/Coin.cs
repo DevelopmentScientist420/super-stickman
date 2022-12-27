e10c7b26fb0bfd1b06fd5aa1f24ea9259719565f
DevelopmentScientist420 <liam.schembri94@gmail.com>
2022-12-27 23:57:06 +0100

    Added coin functionality

---
+++ Assets/Resources/Scripts/Interactables/Coin.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : Interactable
+{
+    private int scoreValue = 5;
+    
+    protected override void Interact()
+    {
+        AddExtraPoints();
+    }
+
+    private void AddExtraPoints()
+    {
+        GameData.PlayerScore += scoreValue;
+        PlayerUI.UpdateScoreText($"Score: {GameData.PlayerScore}");
+        this.gameObject.SetActive(false);
+    }
+}
